93617161b0b0b82274afad5e8ae60317ef9293da
Silas Willis <swill048@wch136-24.cs.ucr.edu>
2021-04-11 22:36:44 -0700

    part 2

--- turnin/swill048_lab3_part2.c
+++ turnin/swill048_lab3_part2.c
@@ -16,18 +16,16 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 
 DDRA = 0x00 ; PORTA = 0xFF ; 
-DDRC = 0xFF ; PORTC = 0x00 ;  
-unsigned char tmpA ;
-unsigned char LEDs ;   
+DDRC = 0xFF ; PORTC = 0x00 ;     
     /* Insert your solution below */
     while (1) {
-tmpA = PINA & 0x0F ; 
-
+unsigned char tmpA = PINA & 0x0F ; 
+unsigned char LEDs = 0x00 ; 
 	if(tmpA == 1 || tmpA == 2){
 	LEDs =  0x60 ;  // 01100000
 	}
 	else if(tmpA ==3 || tmpA == 4){
-	LEDs = 0x7 ; //01110000 
+	LEDs = 0x70 ; //01110000 
 	}
 	else if(tmpA == 5 ||tmpA == 6){
 	LEDs = 0x38 ; 
@@ -35,11 +33,14 @@ tmpA = PINA & 0x0F ;
 	else if(tmpA == 7 || tmpA == 8|| tmpA == 9 ){
 	LEDs = 0x3C ; 
 	}
-	else if(tmpA == 11 || tmpA == 10){
+	else if(tmpA == 12 ||  tmpA == 11 || tmpA == 10){
 	LEDs = 0x3E ; 
 	}
-	else{
+	else if( tmpA > 12 ){
 	LEDs = 0x3E ; 
+	}
+	else if(tmpA == 0) {
+	LEDs = 0x40 ; 
 	} 
 PORTC = LEDs ;
 	
