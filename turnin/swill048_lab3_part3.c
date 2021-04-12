1fc95d2b44233871644c4e4b52188c7cd4a76240
Silas Willis <swill048@wch136-24.cs.ucr.edu>
2021-04-11 22:54:07 -0700

    3

---
+++ turnin/swill048_lab3_part3.c
@@ -0,0 +1,55 @@
+/*	Author: swill048
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+DDRA = 0x00 ; PORTA = 0xFF ; 
+DDRC = 0xFF ; PORTC = 0x00 ;     
+    /* Insert your solution below */
+    while (1) {
+unsigned char tmpA = PINA & 0x0F ; 
+unsigned char LEDs = 0x00 ; 
+	if(tmpA == 1 || tmpA == 2){
+	LEDs =  0x60 ;  // 01100000
+	}
+	else if(tmpA ==3 || tmpA == 4){
+	LEDs = 0x70 ; //01110000 
+	}
+	else if(tmpA == 5 ||tmpA == 6){
+	LEDs = 0x38 ; 
+	}
+	else if(tmpA == 7 || tmpA == 8|| tmpA == 9 ){
+	LEDs = 0x3C ; 
+	}
+	else if(tmpA == 12 ||  tmpA == 11 || tmpA == 10){
+	LEDs = 0x3E ; 
+	}
+	else if( tmpA > 12 ){
+	LEDs = 0x3E ; 
+	}
+	else if(tmpA == 0) {
+	LEDs = 0x40 ; 
+	}
+
+	if((tmpA & 0xF0) == 0x30){  //00110000
+	LEDs = LEDs | 0x80 ; //10000000
+	} 
+
+PORTC = LEDs ;
+	
+	
+    }
+    return 0;
+}
