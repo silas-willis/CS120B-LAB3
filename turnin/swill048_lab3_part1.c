749f8a664c1200eeadfd8dff474ee7892a8e1532
Silas Willis <swill048@wch136-24.cs.ucr.edu>
2021-04-11 21:30:10 -0700

    part 1

---
+++ turnin/swill048_lab3_part1.c
@@ -0,0 +1,46 @@
+/*	Author: swill048
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+DDRA = 0x00 ; PORTA = 0xFF ; 
+DDRB = 0x00 ; PORTB = 0xFF ;
+DDRC = 0xFF ; PORTC = 0x00 ;  
+unsigned char tmpB = 0x00 ; 
+unsigned char tmpA = 0x00 ;  
+unsigned char counter = 0x00 ; 
+    /* Insert your solution below */
+    while (1) {
+
+	tmpA = PORTA ; 
+	tmpB = PORTB ; 
+unsigned char i = 0x00 ; 
+	for(i  ; i < 8 ; ++i){
+
+	if((tmpA & 0x01 ) == 0x01) {
+	counter++ ; 	
+	}
+	if((tmpB & 0x01) == 0x01){
+	counter++ ; 
+	}
+	tmpA = tmpA >> 1 ; 
+	tmpB = tmpB >> 1 ; 
+	
+	}
+
+PORTC = counter ; 
+    }
+    return 0;
+}
